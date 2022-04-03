0c4d7873952801bb0a017e005b44dad65cbb4aec
MasterFiyeur <theo5julien@outlook.fr>
2022-04-03 11:16:19 +0200

    removing json printing

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ def receive_message():
     else:
         # get whatever message a user sent the bot
        output = request.get_json()
-       print(json.dumps(output,separators=(',', ':')))
        for event in output['entry']:
           messaging = event['messaging']
           for message in messaging:
