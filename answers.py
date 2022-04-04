3163c2790970dd451dcf3cb039cabfe992b9deab
ZRunner <z.runner.mc@gmail.com>
2022-04-04 16:47:05 +0200

    cleanup

--- answers.py
+++ answers.py
@@ -1,19 +1,24 @@
 import csv
 from typing import Optional
+
 import spacy
 from spacy.tokens import Doc
 
+
 class AnswersManager:
+    "Class used to answer custom questions (about carbonscore, ecology and other things)"
 
     def __init__(self):
         self.nlp = spacy.load("fr_core_news_lg")
         self.questions: list[tuple[Doc, str]] = []
+        # read questions and related answers from the csv
         with open('answers.csv', 'r', encoding='utf-8') as file:
             reader = csv.reader(file)
             for line in reader:
                 self.questions.append((self.nlp(line[0]), line[1]))
     
     def get_answer(self, question: str) -> Optional[str]:
+        "Get the best corresponding answer from a question, or None if not applicable"
         sentence, max_score = None, 0.5
         question = self.nlp(question)
         for doc, answer in self.questions:
